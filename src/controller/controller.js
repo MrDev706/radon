036dca57269c07111cce72340bdd41878a826e1c
Debashish Dutta <ddutta706@gmail.com>
2022-06-04 16:59:02 +0530

    Mongoose project done

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,14 +1,17 @@
-
+const UserModel= require("../models/model")
 
 
 
 
 const createUser =  async function (req, res) {
     let data = req.body
+    let newData = await UserModel.create(data)
+
+
     
     
 
-   res.send({msg: data})
+   res.send({msg: newData})
 };
 
 module.exports.createUser = createUser
--- src/model/model.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-
-const usereSchema = new mongoose.schema(
-
-    
-)
